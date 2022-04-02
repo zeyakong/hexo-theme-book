ebd4ae9ac01f81106b00e14fc092b28f7de4893b
zeya <kong.zeya@gmail.com>
2022-04-02 19:26:29 -0400

    customization

--- source/js/book-sidebar.js
+++ source/js/book-sidebar.js
@@ -1,34 +1,37 @@
 // collapse sidebar
 var i = 0;
-document.querySelectorAll('.book-sidebar > .sidebar-content > ul > li > ul > li').forEach(function(li){
-    var ul = li.querySelector('ul');
-    if(ul){
-        var accordion = document.createElement('div');
-        accordion.setAttribute("class", "accordion");
-
-        var checkbox = document.createElement('input');
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("id", "accordion-" + i);
-        checkbox.setAttribute("hidden", "");
-            
-        var label = document.createElement('label');
-        label.setAttribute("class", "accordion-header c-hand");
-        label.setAttribute("for", "accordion-" + i);
-        var icon = document.createElement('i');
-        icon.setAttribute("class", "icon icon-caret");
-        label.appendChild(icon);
-
-        var acco_body = document.createElement('div');
-        acco_body.setAttribute("class", "accordion-body")
-        acco_body.appendChild(ul);
-
-        accordion.appendChild(checkbox);
-        accordion.appendChild(acco_body);
-        li.appendChild(label);
-        li.appendChild(accordion);
-        i++;
-    }
-})
+document.querySelectorAll('.book-sidebar > .sidebar-content > ul > li ').forEach(function(li){
+    // var ul = li.querySelector('ul');
+    // console.log(li);
+    // li.
+    // li.setAttribute("style","background-color:blue;");
+    // if(ul){
+    //     var accordion = document.createElement('div');
+    //     accordion.setAttribute("class", "accordion");
+    //
+    //     var checkbox = document.createElement('input');
+    //     checkbox.setAttribute("type", "checkbox");
+    //     checkbox.setAttribute("id", "accordion-" + i);
+    //     checkbox.setAttribute("hidden", "");
+    //
+    //     var label = document.createElement('label');
+    //     label.setAttribute("class", "accordion-header c-hand");
+    //     label.setAttribute("for", "accordion-" + i);
+    //     var icon = document.createElement('i');
+    //     icon.setAttribute("class", "icon icon-caret");
+    //     label.appendChild(icon);
+    //
+    //     var acco_body = document.createElement('div');
+    //     acco_body.setAttribute("class", "accordion-body");
+    //     acco_body.appendChild(ul);
+    //
+    //     accordion.appendChild(checkbox);
+    //     accordion.appendChild(acco_body);
+    //     li.appendChild(label);
+    //     li.appendChild(accordion);
+    //     i++;
+    // }
+});
 
 // highlight current tab
 document.querySelectorAll('.sidebar-content a').forEach(function(item){
@@ -45,6 +48,6 @@ document.querySelectorAll('.sidebar-content a').forEach(function(item){
             parent.querySelector('input').setAttribute("checked", "");
         }
     }
-})
+});
 
 document.getElementsByClassName("sidebar-content")[0].style.display = "block";
